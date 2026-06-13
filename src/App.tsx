afc718e0e9f93e59790fb55ac1c70f253a503340
hackdanismo <office@justtoconfirm.co.uk>
2026-06-13 16:13:49 +0100

    feat: add initial button component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from "react"
 
+import Button from "./components/button"
+
 function App() {
 
   return (
     <>
       <h1>Hello, World</h1>
+      <Button label="Button" />
     </>
   )
 }
